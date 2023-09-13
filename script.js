ed445537649bf2bd9dba8c208a284dfffa9a4ca4
Diego Yañez-Laguna <67990128+diegoyl@users.noreply.github.com>
2023-09-13 15:20:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(function() {
+//   $('.hoverPath').hover(function() {
+//     $('.text1').css('color', 'var(--superset-pink)');
+//     $('.text2').css('color', 'var(--superset-pink)');
+//     $('body').css('background-color', 'white');
+//   }, function() {
+//     // on mouseout, reset the background colour
+//     $('.text1').css('color', 'white');
+//     $('.text2').css('color', 'white');
+//     $('body').css('background-color', 'var(--superset-pink)');
+//   });
+// });
